95dcb39db22ce9c78e53fe944cfaefeac11685dc
RejoiceFiamor <rejoicemawusi57@gmail.com>
2024-06-24 06:11:34 -0700

    adding all file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+// let age= 18
+// let hometown= Abor
+// // alert(' I love singing');
+// let name = prompt('What is your name');
+// alert("Hi " + name + ", welcome to our website.");
+// alert(`Hi ${name}, welcome to our website`)
+
+let title= prompt("What title do you prefer?");
+let name= prompt("What your name?");
+let product= prompt("What is your kind prefer product?");
+// alert("name");
+// alert("Hi + name +", welcome to our website.);
+alert(`Hi ${title}, ${name}, do you like ${product}`);
+
+
+
