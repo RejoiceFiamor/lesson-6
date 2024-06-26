2ccdaa4fd1143a73edebd4734954321ef1cb1f6d
RejoiceFiamor <rejoicemawusi57@gmail.com>
2024-06-26 04:56:50 -0700

    added console message

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@
 // alert("Hi " + name + ", welcome to our website.");
 // alert(`Hi ${name}, welcome to our website`)
 
-let title= prompt("What title do you prefer?");
-let name= prompt("What your name?");
-let product= prompt("What is your kind prefer product?");
-// alert("name");
-// alert("Hi + name +", welcome to our website.);
-alert(`Hi ${title}, ${name}, do you like ${product}`);
+// let title= prompt("What title do you prefer?");
+// let name= prompt("What your name?");
+// let product= prompt("What is your kind prefer product?");
+// // alert("name");
+// // alert("Hi + name +", welcome to our website.);
+// alert(`Hi ${title}, ${name} do you like ${product}`);
+
+
+console.log(5);
+let Rejoice = 7;
+
+
+ let name2 = "Mawusi is my name";
+ let sentence = "Hello viwers, " + name2;
+ let sentence1 = `Hi ✌ ${name2}`;
+ console.log(sentence)
+ 
 
 
 
